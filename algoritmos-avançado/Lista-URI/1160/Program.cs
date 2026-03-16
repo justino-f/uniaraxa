e837a8491ca64a92f5b5e45f13698c1801ad689d
justino-f <r.justinoof@gmail.com>
2026-03-16 19:50:44 -0300

    feat(algoritmos-avançado: lista-URI-atividade-1160-1165)

--- algoritmos-avançado/Lista-URI/1160/Program.cs
+++ algoritmos-avançado/Lista-URI/1160/Program.cs
@@ -1,4 +1,5 @@
-﻿using System; 
+﻿using System;
+using System.Globalization;
 
 class URI {
 
@@ -7,21 +8,20 @@ class URI {
     string[] Resultados = new string[T];
     for (int i = 0; i < T; i++)
     {
-      var n = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+      string[] n = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
       int Contador = 0;
-      double PA = n[0];
-      double PB = n[1];
-      double G1 = n[2];
-      double G2 = n[3];
-      while((int) PA <= (int) PB)
+      int PA = int.Parse(n[0]);
+      int PB = int.Parse(n[1]);
+      double G1 = double.Parse(n[2], CultureInfo.InvariantCulture);
+      double G2 = double.Parse(n[3], CultureInfo.InvariantCulture);
+      while(PA <= PB && Contador <= 101)
       {
-        PA += PA * (G1 / 100);
-        PB += PB * (G2 / 100);
+        PA += (int) (PA * (G1 / 100.0));
+        PB += (int) (PB * (G2 / 100.0));
         Contador++;
-        Console.WriteLine(PA);
-        Console.WriteLine(PB);
       }
-      Resultados[i] = $"{Contador} anos.";
+      if (Contador > 100) Resultados[i] = $"Mais de 1 seculo.";
+      else Resultados[i] = $"{Contador} anos.";
     } 
     for (int k = 0; k < T; k++) Console.WriteLine($"{Resultados[k]}");
   }
--- algoritmos-avançado/Lista-URI/1165/Program.cs
+++ algoritmos-avançado/Lista-URI/1165/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; 
+
+class URI {
+
+  static void Main(string[] args) { 
+
+    int X = int.Parse(Console.ReadLine());
+    string[] Resultados = new string[X];
+    for (int i = 0; i < X; i++)
+    {
+      int N = int.Parse(Console.ReadLine()); 
+      int Contador = 0;
+      for (int j = 1; j <= N; j++)
+      {
+        if (N % j == 0) Contador++;
+      }
+      if (Contador == 2) Resultados[i] = $"{N} eh primo";
+      else Resultados[i] = $"{N} nao eh primo";
+    }
+    for (int k = 0; k < X; k++) Console.WriteLine($"{Resultados[k]}");
+  }
+}
