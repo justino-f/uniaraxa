a66dba31caf4105095e119cbb2e1e6e66ab469ed
justino-f <r.justinoof@gmail.com>
2026-08-19 21:57:49 -0300

    feat(desenvolvimento-de-aplicativos-web-e-cloud-computing: Lista01 Ex02 complete)

--- desenvolvimento-de-aplicativos-web-e-cloud-computing/Lista01/Ex02/Controllers/FreightController.cs
+++ desenvolvimento-de-aplicativos-web-e-cloud-computing/Lista01/Ex02/Controllers/FreightController.cs
@@ -5,11 +5,20 @@ using Microsoft.AspNetCore.Mvc;
 [Route("Freight")]
 public class FreightController : ControllerBase
 {
+  Dictionary<double, string> taxtable = new Dictionary<double, string>()
+  {
+    {50.0, "SP"},
+    {55.0, "MG"},
+    {60.0, "RJ"}
+  };
+
   [HttpPost]
   public IActionResult CalculateFreight(string name, double weigth, double heigth, double width, double length, string state)
   {
     double volume = weigth * heigth * width;
     double volumeTax = 0.01 * volume;
-    return Ok("");
+    double stateTax = taxtable.FirstOrDefault(x => x.Value == state).Key;    
+    double tax = volumeTax + stateTax;
+    return Ok($"{tax}");
   }
 }
